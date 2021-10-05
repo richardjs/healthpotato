64929163e336364125d368a8072de4ffcceea2f2
Richard Schneider <richard@schneiderbox.net>
2021-10-05 17:48:09 -0500

    Add basic test of getting weight form

--- healthpotato/tests.py
+++ healthpotato/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from healthpotato.models import WeightData
+
+
+class WeightEntryTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(
+           'testuser', 'testuser@example.com', 'password').save()
+        self.client.login(username='testuser', password='password')
+
+    def test_get_weight_form(self):
+        response = self.client.get('/weight')
+        self.assertTrue(b'weight' in response.content)
+        self.assertTrue(b'submit' in response.content)
