eff4b30db400fe2c74ec92dbe6bc4ed4655173f3
Richard Schneider <richard@schneiderbox.net>
2021-10-26 13:06:30 -0500

    Style entry forms with Bootstrap

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'healthpotato',
+    'django.forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
