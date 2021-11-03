04fd624eddc0736aa37bf9ddbe5b1fdaf272d477
Richard Schneider <richard@schneiderbox.net>
2021-11-02 23:55:58 -0500

    Add log out link

--- project/settings.py
+++ project/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
